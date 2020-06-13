c728b65e0754b13f45905985d606a61d783aa71c
Lucas Matias <lucas.matias.87@gmail.com>
2020-06-13 19:39:41 -0300

    file criation

--- start.js
+++ start.js
@@ -1,7 +1,8 @@
 const csv = require('csv-parser')
 const fs = require('fs')
+const { unnest, keys, concat } = require('ramda');
 
-const articles = [];
+const articles = {};
 const authors = {};
 let currId = 1;
 
@@ -11,6 +12,7 @@ fs.createReadStream('input/amostra.csv')
   .pipe(csv())
   .on('data', (data) => {
     const authorsnames = data.AF.split(';');
+    data.authors = [];
     authorsnames.forEach(authorName => {
       if (!authors[authorName]) {
         authors[authorName] = {};
@@ -19,9 +21,42 @@ fs.createReadStream('input/amostra.csv')
       } else {
         authors[authorName].articles.push(data.PMID);
       }
+      data.authors.push(authors[authorName]);
     })
-    articles.push(data)
+    articles[data.PMID] = data;
   })
   .on('end', () => {
-    console.log(authors);
+    const cites = [];
+    let relation = [];
+    fs.createReadStream('input/cites.csv')
+      .pipe(csv())
+      .on('data', (data) => {
+        if (articles[data.article] && articles[data.cited_by]) {
+          const citedXCiting = unnest(
+            articles[data.article]
+            .authors
+            .map(citedAuthor => {
+              return articles[data.cited_by]
+                      .authors
+                      .map(citingAuthor => {
+                        return [citedAuthor.id, citingAuthor.id];
+                      })
+            })
+          );
+          relation.push(citedXCiting);
+          cites.push(data)
+        }
+      })
+      .on('end', () => {
+        relation = unnest(relation);
+        const authorsNames = keys(authors);
+
+        const networkFile = (
+`*Vertices ${authorsNames.length}
+${authorsNames.map((name) => `${authors[name].id} "${name}"\n`).reduce(concat)}
+*Arcs
+${relation.map(([cited, citing]) => `${cited} ${citing}\n`).reduce(concat)}*Edges`
+);
+        fs.writeFileSync('output/network.net', networkFile);
+      });
   });

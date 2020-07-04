0d6a54434ceb4508a49050ff3782825225c044a6
Lucas Matias <lucas.matias.87@gmail.com>
2020-07-04 16:38:22 -0300

    ajustes rede

--- authors_network.js
+++ authors_network.js
@@ -8,8 +8,8 @@ let currId = 1;
 
 const nextId = () => currId++;
 
-fs.createReadStream('input/sample2.csv')
-  .pipe(csv())
+fs.createReadStream('input/pubmed_filtrado_humanos_enriquecido_QoL.csv')
+  .pipe(csv({ separator: '\t' }))
   .on('data', (data) => {
     const authorsnames = data.AF.split(';');
     data.authors = [];
@@ -31,41 +31,48 @@ fs.createReadStream('input/sample2.csv')
     fs.createReadStream('input/cites.csv')
       .pipe(csv())
       .on('data', (data) => {
-        if (articles[data.article] && articles[data.cited_by]) {
+        const article = articles[data.article];
+        const citingArticle = articles[data.cited_by]
+        if (article && citingArticle) {
+          const citingAuthorsNumber = citingArticle.AU_QTDE;
+          const JCR = citingArticle.JCR_IMPACT_FACTOR || 0;
+          const weight = (JCR / citingAuthorsNumber).toFixed(2);
+
           const citingXCited = unnest(
-            articles[data.article]
+            article
             .authors
             .map(citedAuthor => {
-              return articles[data.cited_by]
+              return citingArticle
                       .authors
                       .map(citingAuthor => {
-                        return [citingAuthor.id, citedAuthor.id];
+                        return [citingAuthor.id, citedAuthor.id, weight];
                       })
             })
+            
           );
           relation.push(citingXCited);
           cites.push(data)
         }
       })
       .on('end', () => {
-        const usedRelations = {};
-        const relationId = ([citing, cited]) => `${citing}x${cited}`;
+        // const usedRelations = {};
+        // const relationId = ([citing, cited]) => `${citing}x${cited}`;
         relation = unnest(relation);
-        relation = relation.filter(rel => {
-          const relId = relationId(rel);
-          if (usedRelations[relId]) {
-            return false;
-          }
-          usedRelations[relId] = true;
-          return true;
-        });
+        // relation = relation.filter(rel => {
+        //   const relId = relationId(rel);
+        //   if (usedRelations[relId]) {
+        //     return false;
+        //   }
+        //   usedRelations[relId] = true;
+        //   return true;
+        // });
         const authorsNames = keys(authors);
 
         const networkFile = (
 `*Vertices ${authorsNames.length}
 ${authorsNames.map((name) => `${authors[name].id} "${name}"\n`).reduce(concat)}
 *Arcs
-${relation.map(([citing, cited]) => `${citing} ${cited}\n`).reduce(concat, '')}*Edges`
+${relation.map(([citing, cited, weight]) => `${citing} ${cited} ${weight}\n`).reduce(concat, '')}*Edges`
 );
         fs.writeFileSync('output/network.net', networkFile);
       });
